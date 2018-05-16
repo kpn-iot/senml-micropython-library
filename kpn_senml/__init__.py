a60b22d1c079188042eaac897b8d5d3564daa966
PORTABLE\janbo <jbogaerts@myforce.be>
2018-05-16 13:58:20 +0200

    add licens and header to source files

--- kpn_senml/__init__.py
+++ kpn_senml/__init__.py
@@ -1,3 +1,17 @@
+# _  __  ____    _   _ 
+# | |/ / |  _ \  | \ | |
+# | ' /  | |_) | |  \| |
+# | . \  |  __/  | |\  |
+# |_|\_\ |_|     |_| \_|
+# 
+# (c) 2018 KPN
+# License: GNU General Public License v3.0.
+# Author: Jan Bogaerts
+# 
+# include all
+
+
+
 from .senml_base import SenmlBase
 from .senml_pack import SenmlPack
 from .senml_record import SenmlRecord
